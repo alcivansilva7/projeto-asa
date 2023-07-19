22f7516738ee7e5b9cb2191057b0475f128bd934
Alcivan Silva <alcivansilva7@gmail.com>
2023-07-19 13:21:52 -0300

    Criando usuário para acesso ao segundo banco

--- banco/iniciar.sh
+++ banco/iniciar.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-mysql -uroot -proot -e "CREATE DATABASE intranet;"
+mysql -uroot -proot -e "CREATE USER 'intranet'@'localhost' IDENTIFIED BY 'intranet';"
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON * . * TO 'intranet'@'localhost';"
+mysql -uroot -proot -e "FLUSH PRIVILEGES;"
+mysql -uintranet -pintranet -e "CREATE DATABASE intranet;"
