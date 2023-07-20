8f48c2ff8abd7f1e2435bf083f9ec2a910e17856
Alcivan Silva <alcivansilva7@gmail.com>
2023-07-20 12:04:10 -0300

    implementando serviços da intranet

--- banco/iniciar.sh
+++ banco/iniciar.sh
@@ -4,3 +4,4 @@ mysql -uroot -proot -e "CREATE USER 'intranet'@'localhost' IDENTIFIED BY 'intran
 mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON * . * TO 'intranet'@'localhost';"
 mysql -uroot -proot -e "FLUSH PRIVILEGES;"
 mysql -uintranet -pintranet -e "CREATE DATABASE intranet;"
+mysql -uintranet -pintranet -e "CREATE DATABASE nextcloud;"
