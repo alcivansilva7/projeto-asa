cb4b42821d8aaac1be134b5b64535ec867085d3a
Alcivan Silva <alcivansilva7@gmail.com>
2023-07-25 20:29:32 -0300

    alterações de ultima hora

--- banco/init.sql
+++ banco/init.sql
@@ -1,17 +1,12 @@
-CREATE USER IF NOT EXISTS intranet@'%' IDENTIFIED BY 'intranet';
-SET PASSWORD FOR intranet@'%' = PASSWORD('intranet');
-
-CREATE USER IF NOT EXISTS nextcloud@'%' IDENTIFIED BY 'nextcloud';
-SET PASSWORD FOR nextcloud@'%' = PASSWORD('nextcloud');
-
-CREATE USER IF NOT EXISTS glpi@'%' IDENTIFIED BY 'glpi';
-SET PASSWORD FOR glpi@'%' = PASSWORD('glpi');
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin';
+GRANT ALL PRIVILEGES ON * . * TO 'admin'@'%';
+FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS intranet;
-GRANT ALL ON intranet.* TO intranet@'%'
+GRANT ALL ON intranet.* TO 'admin'@'%';
 
-CREATE DATABASE IF NOT EXISTS inextcloud;
-GRANT ALL ON nextcloud.* TO nextcloud@'%'
+CREATE DATABASE IF NOT EXISTS nextcloud;
+GRANT ALL ON nextcloud.* TO 'admin'@'%';
 
 CREATE DATABASE IF NOT EXISTS glpi;
-GRANT ALL ON glpi.* TO glpi@'%'
+GRANT ALL ON glpi.* TO 'admin'@'%';
